50a60e47996b842fd23f1947b94884769eb247e6
Spencer Croy <spencerccroy@gmail.com>
2022-09-18 14:08:13 -0700

    rearrage CSS and update Location cards

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 var map;
 
-
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
@@ -9,7 +8,7 @@ function initMap() {
 }
 
 
-// Add location selected to the list.
+
 
 const addLocationButton = document.getElementById("dropbtn");
 addLocationButton.addEventListener("click", addLocationToList)
@@ -21,13 +20,15 @@ function addLocationToList() {
   let locationCard = document.createElement("div")
   locationCard.classList.add("locationCard")
 
-  // location card title
+  // Location card title
   let title = document.createElement("h1");
   title.innerText = "You've added " + location + " to your itinerary";
   title.classList.add("locationCardTitle");
   locationCard.appendChild(title);
 
-  //Add elementcard to list
+  
+
+  //Add Location card to list
   document.getElementById("locationList").appendChild(locationCard);
 
 }
